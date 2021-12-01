7204fc5e5ae132521b72e8375da5f60d7942b142
David <david.cabello.info@gmail.com>
2021-12-01 10:11:28 +0100

    depurando codigo

--- src/main/java/com/dscode/aplication/controller/TestController.java
+++ src/main/java/com/dscode/aplication/controller/TestController.java
@@ -12,38 +12,25 @@ import io.micrometer.core.instrument.MeterRegistry;
 @RestController
 public class TestController {
 
-	private Counter counterCel;
-	private Counter counterFar;
+	private static Counter counterCel;
+	private static Counter counterFar;
 	
 	
 	private final static Logger logger= LoggerFactory.getLogger(TestController.class);
 	
-	public TestController(MeterRegistry registro) {
-		this.counterCel = Counter.builder("invocaciones.Celsius").description("Invocaciones totales").register(registro);
-		this.counterFar = Counter.builder("invocaciones.Fahrenheit").description("Invocaciones totales").register(registro);
-	}
-	
-	@GetMapping(path="/testCelsius")
-	public String testCelsius() {
-		counterCel.increment();
-		return "Celsius";
-	}
-	
-	@GetMapping(path="/testFarhen")
-	public String holaMundo() {
-		counterFar.increment();
-		return "Fahrenheit";
-	}
-	
 	@GetMapping(path="/celAFar/{celsius}")
 	public static float celsiusAFahrenheit(@PathVariable float celsius) {
-		logger.info("Llamada al endpoint celsius.");
+		counterCel.increment();
+		logger.info("Llamadas al endpoint celsius:" + counterCel.count());
+		
 		return (celsius * 1.8f) + 32;
 	}
 	
 	@GetMapping(path="/farACel/{fahrenheit}")
 	public static float fahrenheitACelsius(@PathVariable float fahrenheit) {
-		logger.info("Llamada al endpoint fahrenheit.");
+		counterFar.increment();
+		logger.info("Llamadas al endpoint fahrenheit:" + counterFar.count());
+		
 		return (fahrenheit - 32) / 1.8f;
 	}
 }
